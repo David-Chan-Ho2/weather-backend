369326bb0c8288ab9efa7cd66c4e715e62ed2f06
David-Chan-Ho2 <dho12@charlotte.edu>
2025-08-02 01:47:01 -0400

    refactor functions

--- index.js
+++ index.js
@@ -18,40 +18,49 @@ const THINGSPEAK_CHANNEL_ID = process.env.THINGSPEAK_CHANNEL_ID;
 const THINGSPEAK_API_KEY = process.env.THINGSPEAK_API_KEY;
 
 // Get data
+const getDataAPI = async () => {
+  const { data } = await axios.get(
+    `https://api.thingspeak.com/channels/${THINGSPEAK_CHANNEL_ID}/feeds.json?api_key=${THINGSPEAK_API_KEY}&results=8000`
+  );
+
+  return data;
+};
+
+const convertData = (feeds) => {
+  const dayMap = {};
+
+  feeds.forEach((f) => {
+    const date = new Date(f.created_at);
+    const dayKey = date.toISOString().slice(0, 10); // "YYYY-MM-DD"
+    const temp = parseFloat(f.field1);
+    const humid = parseFloat(f.field2);
+    if (isNaN(temp)) return;
+
+    if (!dayMap[dayKey]) {
+      dayMap[dayKey] = { tempSum: 0, humidSum: 0, count: 0 };
+    }
+
+    dayMap[dayKey].tempSum += temp;
+    if (!isNaN(humid)) dayMap[dayKey].humidSum += humid;
+    dayMap[dayKey].count++;
+  });
+
+  const daily = Object.entries(dayMap).map(
+    ([date, { tempSum, humidSum, count }]) => ({
+      date,
+      avgTemp: parseFloat((tempSum / count).toFixed(2)),
+      avgHumidity: parseFloat((humidSum / count).toFixed(2)),
+    })
+  );
+
+  return daily;
+};
+
 const getData = async (req, res) => {
   try {
-    const { data } = await axios.get(
-      `https://api.thingspeak.com/channels/${THINGSPEAK_CHANNEL_ID}/feeds.json?api_key=${THINGSPEAK_API_KEY}&results=8000`
-    );
-
+    const data = await getDataAPI();
     const feeds = data.feeds;
-
-    const dayMap = {};
-
-    feeds.forEach((f) => {
-      const date = new Date(f.created_at);
-      const dayKey = date.toISOString().slice(0, 10); // "YYYY-MM-DD"
-      const temp = parseFloat(f.field1);
-      const humid = parseFloat(f.field2);
-      if (isNaN(temp)) return;
-
-      if (!dayMap[dayKey]) {
-        dayMap[dayKey] = { tempSum: 0, humidSum: 0, count: 0 };
-      }
-
-      dayMap[dayKey].tempSum += temp;
-      if (!isNaN(humid)) dayMap[dayKey].humidSum += humid;
-      dayMap[dayKey].count++;
-    });
-
-    const daily = Object.entries(dayMap).map(
-      ([date, { tempSum, humidSum, count }]) => ({
-        date,
-        avgTemp: parseFloat((tempSum / count).toFixed(2)),
-        avgHumidity: parseFloat((humidSum / count).toFixed(2)),
-      })
-    );
-
+    const daily = convertData(feeds);
     res.json(daily);
   } catch (err) {
     res.status(500).json({ error: "Failed to fetch or process data" });
